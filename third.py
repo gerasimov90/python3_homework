509295d95c68f730a6f154700e1aa7e2fde46bc0
GerasimovVictor <gerasimov.viktor90@gmail.com>
2023-03-04 12:19:04 +0700

    task 1

--- third.py
+++ third.py
@@ -1,3 +1,8 @@
 #Задача 16
 count = 0
 some_list = [int(input('Введите элемент списка: ')) for _ in range(int(input('Введите количество элементов в списке: ')))]
+x = int(input('Введите значение элемента для поиска: '))
+for i in range(0, len(some_list) - 1):
+    if some_list[i] == x:
+        count += 1
+print(count)
